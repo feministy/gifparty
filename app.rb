1950c1e96eed88a3e8edfbaa7670760f2e04ed99
Liz Abinante <eabinante@gmail.com>
2014-05-07 13:44:25 -0500

    removed slim templates

--- app.rb
+++ app.rb
@@ -29,12 +29,72 @@ helpers do
 end
 
 get '/' do
-  slim :index
+  erb :index
 end
 
 get '/angry' do
   @imgs = images_for('angry/')
   @emotion = "Rawr! Angry!"
+  erb :gifs
+end
+
+get '/annoyed' do
+  @imgs = images_for('annoyed/')
+  @emotion = "Srsly. So annoyed."
+  erb :gifs
+end
+
+get '/comforting' do
+  @imgs = images_for('comforting/')
+  @emotion = "No 'fraid, lil soft soft."
+  erb :gifs
+end
+
+get '/cute' do
+  @imgs = images_for('cute/')
+  @emotion = "SO PRECIOUS!"
+  erb :gifs
+end
+
+get '/dancing' do
+  @imgs = images_for('dancing/')
+  @emotion = "My milkshake brings all the boys to the yard."
+  erb :gifs
+end
+
+get '/evil' do
+  @imgs = images_for('evil/')
+  @emotion = "Cackle."
+  erb :gifs
+end
+
+get '/fuck_you' do
+  @imgs = images_for('fuck_you/')
+  @emotion = "FUCK YOU MOTHER FUCKER."
+  erb :gifs
+end
+
+get '/funny' do
+  @imgs = images_for('funny/')
+  @emotion = "Giggles!"
+  erb :gifs
+end
+
+get '/happy' do
+  @imgs = images_for('happy/')
+  @emotion = "Eeeeeeeeeeee!"
+  erb :gifs
+end
+
+get '/victory' do
+  @imgs = images_for('victory/')
+  @emotion = "I win."
+  erb :gifs
+end
+
+get '/whatever' do
+  @imgs = images_for('whatever/')
+  @emotion = "Whatever whatever."
   erb :gifs
 end
 
