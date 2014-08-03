cbfa07f09d570f33d12175905d7afa9dd757bccb
Liz Abinante <eabinante@gmail.com>
2014-08-03 17:21:08 -0500

    returning arrays for all routes

--- app.rb
+++ app.rb
@@ -38,65 +38,65 @@ end
 
 get '/api/annoyed.json' do
   imgs = json_for('annoyed/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/comforting.json' do
   imgs = json_for('comforting/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/cute.json' do
   imgs = json_for('cute/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/dancing.json' do
   imgs = json_for('dancing/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/evil.json' do
   imgs = json_for('evil/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/fuck_you.json' do
   imgs = json_for('fuck_you/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/funny.json' do
   imgs = json_for('funny/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/happy.json' do
   imgs = json_for('happy/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/nope.json' do
   imgs = json_for('nope/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/sad.json' do
   imgs = json_for('sad/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/victory.json' do
   imgs = json_for('victory/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/whatever.json' do
   imgs = json_for('whatever/')
-  json images: imgs
+  json imgs
 end
 
 get '/api/wtf.json' do
   imgs = json_for('wtf/')
-  json images: imgs
+  json imgs
 end
